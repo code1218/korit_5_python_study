b321a3298a6e24e28c5958cb2f791b5472abc220
code1218 <skjil1218@gmail.com>
2024-04-16 15:58:01 +0900

    수업자료 업로드

---
+++ class_study/BookRepository.py
@@ -0,0 +1,40 @@
+import pymysql
+import pymysql.cursors
+
+class BookRepository:
+
+    host = ""
+    port = 3306
+    user = ""
+    password = ""
+    database = ""
+
+    @classmethod
+    def saveBook(cls, book=None):
+
+        connection = pymysql.connect(
+            host=cls.host,
+            port=cls.port,
+            user=cls.user,
+            password=cls.password,
+            database=cls.database)
+        
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        sql = f'''
+            insert into book_tb
+            values (0, 
+                %s,
+                %s,
+                %s,
+                %s,
+                1,
+                4,
+                "http://test.com/test.png",
+                now(),
+                now()
+            )        
+        '''
+        cursor.execute(sql, (book.bookName, book.authorName, book.publisherName, book.isbn))
+        connection.commit() 
+
+        pass
